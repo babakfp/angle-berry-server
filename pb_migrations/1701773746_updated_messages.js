791824bce6cf0f916e7cd30c2d6d2ec1c27dd36b
babakfp <babak.bxf@gmail.com>
2023-12-05 02:56:59 -0800

    Reverced order

---
+++ pb_migrations/1701773746_updated_messages.js
@@ -0,0 +1,20 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("86i2iracs6xus87")
+
+  collection.createRule = "@request.auth.id = user"
+  collection.updateRule = "@request.auth.id = user"
+  collection.deleteRule = "@request.auth.id = user"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("86i2iracs6xus87")
+
+  collection.createRule = "user = @request.auth.id"
+  collection.updateRule = "user = @request.auth.id"
+  collection.deleteRule = "user = @request.auth.id"
+
+  return dao.saveCollection(collection)
+})
